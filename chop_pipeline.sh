7fca2d051659dac941a4c919d3d5e7d27bcdac7b
Agustín Sánchez Belmonte <70705333+agsanbel@users.noreply.github.com>
2023-04-11 09:18:41 +0200

    Update chop_pipeline.sh

--- chop_pipeline.sh
+++ chop_pipeline.sh
@@ -4,7 +4,7 @@ for i in $@
 do
     mkdir results/$i
 
-    ./chopchop.py -G hg38 -o results/$i -Target $i --scoringMethod DOENCH_2016 -consensusUnion > results/$i/$i.txt
+    ./chopchop.py -G hg38 -o results/$i -Target $i --scoringMethod DOENCH_2016 -consensusUnion -t CODING > results/$i/$i.txt
 
     echo "$i done!"
 done
